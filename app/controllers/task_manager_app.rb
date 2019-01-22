69049e94ceacd7878f8d7edd9fa67c430bed317e
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-21 22:26:23 -0700

    Added methods to edit tasks

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -25,5 +26,15 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id' do
     @task = Task.find(params[:id])
     erb :show
-  end 
+  end
+
+  get '/task/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -22,4 +22,16 @@ class Task
     database.results_as_hash = true
     database
   end
+
+  def self.update(id, task_params)
+    database.execute("UPDATE tasks
+                      SET title = ?,
+                          description = ?
+                      WHERE id = ?;",
+                      task_params[:title],
+                      task_params[:description],
+                      id)
+
+    Task.find(id)
+  end
 end
